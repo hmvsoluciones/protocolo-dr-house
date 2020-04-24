1a9b9a38516371f5d2dc39292c234fd84b408538
hmv.soluciones <hmv.soluciones@gmail.com>
2020-04-23 21:19:12 -0500

    Actualizacion de etiquetas

--- components/binnacle/index.php
+++ components/binnacle/index.php
@@ -16,19 +16,19 @@ require_once __DIR__ . '../../../src/constants/constants.php';
 
             <form class="form-inline" id="frmBinnacleSearch">
                 <div class="form-group mx-sm-3 mb-2">
-                    <label for="binnacleDate" >Fecha</label>
+                    <label for="binnacleDate" ><?php echo BINNACLE_OPERATION_DATE; ?></label>
                     <input type="text" readonly name="binnacleDate" size="10"  id="binnacleDate" placeholder="" class="validate[required]">
                 </div>
                 <div class="form-group mx-sm-3 mb-2">
-                    <label for="catalogTable">Table</label>
+                    <label for="catalogTable"><?php echo BINNACLE_TABLE; ?></label>
                     <select class="form-control" name="catalogTable" id="catalogTable"></select>
                 </div>
                 <div class="form-group mx-sm-3 mb-2">
-                    <label for="catalogOperation">operacion</label>
+                    <label for="catalogOperation"><?php echo BINNACLE_OPERATION; ?></label>
                     <select class="form-control" name="catalogOperations" id="catalogOperations"></select>
                 </div>
                 <div class="form-group mx-sm-3 mb-2">
-                    <label for="usuario">User</label>
+                    <label for="usuario"><?php echo BINNACLE_OPERATION_USER; ?></label>
                     <select class="form-control" name="user" id="user"></select>
                 </div>
                 <span class="fa fa-search iconSuccess" data-toggle="modal" onclick="binnacleSearch()"></span>
